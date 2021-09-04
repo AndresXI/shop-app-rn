7e1805b5023d67033c3b3094f4886b94e962ffbd
andres alcocer <andresalcocer7@yahoo.com>
2021-09-03 21:19:58 -0400

    now able to crate and update products via admin page

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,15 +1,17 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import { View, Text, ScrollView, TextInput, StyleSheet } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import HeaderButton from '../../components/UI/HeaderButton'
+import * as productActions from '../../store/actions/products'
 
 const EditProductScreen = (props) => {
   const prodId = props.navigation.getParam('productId')
   const editedProduct = useSelector((state) =>
     state.products.userProducts.find((prod) => prod.id === prodId)
   )
+  const dispatch = useDispatch()
 
   const [title, setTitle] = useState(editedProduct ? editedProduct.title : '')
   const [price, setPrice] = useState('')
@@ -21,8 +23,16 @@ const EditProductScreen = (props) => {
   )
 
   const submitHandler = useCallback(() => {
-    console.log('Sumbitt')
-  }, [])
+    if (editedProduct) {
+      dispatch(
+        productActions.updateProduct(prodId, title, description, imageUrl)
+      )
+    } else {
+      dispatch(
+        productActions.createProduct(title, description, imageUrl, +price)
+      )
+    }
+  }, [dispatch, prodId, title, description, imageUrl, price])
 
   useEffect(() => {
     props.navigation.setParams({ submit: submitHandler })
@@ -35,7 +45,7 @@ const EditProductScreen = (props) => {
           <Text style={styles.label}>Title</Text>
           <TextInput
             value={title}
-            onChange={(text) => setTitle(text)}
+            onChangeText={(text) => setTitle(text)}
             style={styles.input}
           />
         </View>
@@ -44,7 +54,7 @@ const EditProductScreen = (props) => {
           <Text style={styles.label}>Image URL</Text>
           <TextInput
             value={imageUrl}
-            onChange={(imageUrl) => setImageUrl(imageUrl)}
+            onChangeText={(imageUrl) => setImageUrl(imageUrl)}
             style={styles.input}
           />
         </View>
@@ -54,7 +64,7 @@ const EditProductScreen = (props) => {
             <Text style={styles.label}>Price</Text>
             <TextInput
               value={price}
-              onChange={(price) => setPrice(price)}
+              onChangeText={(price) => setPrice(price)}
               style={styles.input}
             />
           </View>
@@ -64,7 +74,7 @@ const EditProductScreen = (props) => {
           <Text style={styles.label}>Description</Text>
           <TextInput
             value={description}
-            onChange={(description) => setDescription(description)}
+            onChangeText={(description) => setDescription(description)}
             style={styles.input}
           />
         </View>
