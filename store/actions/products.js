da0263122b737df3173999c57142df253f6c63eb
andres alcocer <andresalcocer7@yahoo.com>
2021-09-10 17:44:53 -0400

    now able to delete products from server

--- store/actions/products.js
+++ store/actions/products.js
@@ -37,9 +37,18 @@ export const fetchProducts = () => {
 }
 
 export const deleteProduct = (productId) => {
-  return {
-    type: DELETE_PRODUCT,
-    productId,
+  return async (dispatch) => {
+    await fetch(
+      `https://rn-shop-3881e-default-rtdb.firebaseio.com/products/${productId}.json`,
+      {
+        method: 'DELETE',
+      }
+    )
+
+    dispatch({
+      type: DELETE_PRODUCT,
+      productId,
+    })
   }
 }
 
@@ -71,13 +80,26 @@ export const createProduct = (title, description, imageUrl, price) => {
 }
 
 export const updateProduct = (id, title, description, imageUrl) => {
-  return {
-    type: UPDATE_PRODUCT,
-    productId: id,
-    productData: {
-      title,
-      description,
-      imageUrl,
-    },
+  return async (dispatch) => {
+    await fetch(
+      `https://rn-shop-3881e-default-rtdb.firebaseio.com/products/${id}.json`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ title, description, imageUrl }),
+      }
+    )
+
+    dispatch({
+      type: UPDATE_PRODUCT,
+      productId: id,
+      productData: {
+        title,
+        description,
+        imageUrl,
+      },
+    })
   }
 }
