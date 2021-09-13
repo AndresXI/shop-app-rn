65b7c7cfae03896032630d8b40a823455aeeedd1
andres alcocer <andresalcocer7@yahoo.com>
2021-09-13 19:27:17 -0400

    created AuthScreen component

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createAppContainer } from 'react-navigation'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 import { Platform } from 'react-native'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createDrawerNavigator } from 'react-navigation-drawer'
@@ -12,6 +12,7 @@ import ProductDetailScreen from '../screens/shop/ProductDetailScreen'
 import OrdersScreen from '../screens/shop/OrdersScreen'
 import UserProductsScreen from '../screens/user/UserProductsScreen'
 import EditProductScreen from '../screens/user/EditProductScreen'
+import AuthScreen from '../screens/user/AuthScreen'
 
 const defaultNavOptions = {
   headerStyle: {
@@ -100,4 +101,18 @@ const ShopNavigator = createDrawerNavigator(
   }
 )
 
-export default createAppContainer(ShopNavigator)
+const AuthNavigator = createStackNavigator(
+  {
+    Auth: AuthScreen,
+  },
+  {
+    defaultNavigationOptions: defaultNavOptions,
+  }
+)
+
+const MainNavigator = createSwitchNavigator({
+  Auth: AuthNavigator,
+  Shop: ShopNavigator,
+})
+
+export default createAppContainer(MainNavigator)
